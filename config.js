80cf734e51e5299b2ee9a154933508c96a78913a
Kamil Ali <kamil.m.ali@outlook.com>
2021-12-01 11:13:58 -0800

    updated payment address to kovan

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const impactNFTAddress = "0xa96548ed600572b17Fb6378f71a8F23438aD4540";
 export const impactMarketAddress = "0xfacC602284dD05Bcf8149dac4F9baa55BF09a4CE";
-export const impactPaymentAddress = "0x49Ce3A93FeFB1A652F9dfCC5bc167f49e327e206";
+export const impactPaymentAddress = "0xB93f2C4F112a2d4aDFF731Aa92E882aC1c2000A4";
 // not used anymore
 export const impactPaymasterAddress = "0x5620992B3414e54d29E7Ed395dE8C16438FAe103";
