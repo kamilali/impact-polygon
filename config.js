bc2cea7d63011afa149cdf38795c583d6a4a4bfd
Kamil Ali <kamil.m.ali@outlook.com>
2021-12-01 11:10:14 -0800

    updated NFT contract address

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-export const impactNFTAddress = "0xB9ed574Bb9275361AED12bb0695573F479F42C8E";
-export const impactMarketAddress = "0xfDd22Fce7C9835aDd37CF13f91d4F19F50Fc4A09";
+export const impactNFTAddress = "0xa96548ed600572b17Fb6378f71a8F23438aD4540";
+export const impactMarketAddress = "0xfacC602284dD05Bcf8149dac4F9baa55BF09a4CE";
 export const impactPaymentAddress = "0x49Ce3A93FeFB1A652F9dfCC5bc167f49e327e206";
 // not used anymore
 export const impactPaymasterAddress = "0x5620992B3414e54d29E7Ed395dE8C16438FAe103";
