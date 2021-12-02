40773085c77637aab6e7abb030117c7ac6434985
Kamil Ali <kamil.m.ali@outlook.com>
2021-12-02 13:00:18 -0800

    redeployed contract

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-export const impactNFTAddress = "0xa96548ed600572b17Fb6378f71a8F23438aD4540";
-export const impactMarketAddress = "0xfacC602284dD05Bcf8149dac4F9baa55BF09a4CE";
+export const impactNFTAddress = "0x219a20c9d5267D9B76f688da07469D97e20E2763";
+export const impactMarketAddress = "0xC6CA9aC35f5e3e14957F92D00AFb1e5fd1491bd4";
 export const impactPaymentAddress = "0xB93f2C4F112a2d4aDFF731Aa92E882aC1c2000A4";
 // not used anymore
 export const impactPaymasterAddress = "0x5620992B3414e54d29E7Ed395dE8C16438FAe103";
