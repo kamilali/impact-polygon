b876a3808803b1acd71d97285c7a35427c474dc6
Kamil Ali <kamil.m.ali@outlook.com>
2021-12-04 23:00:22 -0800

    deployed payment contract on eth

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const impactNFTAddress = "0xe9c66bc5c792e7b1350105A467E3e41F13FEc5f2";
 export const impactMarketAddress = "0x11F3A6AE739EA43B28e0b37E8424d296d52d07d6";
-export const impactPaymentAddress = "0xB93f2C4F112a2d4aDFF731Aa92E882aC1c2000A4";
+export const impactPaymentAddress = "0xa810244e9222932Ae8E12b8F90D14fEA59630D11";
 // not used anymore
 export const impactPaymasterAddress = "0x5620992B3414e54d29E7Ed395dE8C16438FAe103";
